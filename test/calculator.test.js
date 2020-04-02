effa50eb1c2c7515f924d1293d238c316c1a2928
Carol Adão <caroline@forlogic.net>
2020-04-01 22:22:32 -0300

    developer test

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -6,7 +6,16 @@ const calculator = new Calculator();
 
 describe( 'Calculator', () => {
   test( '', () => {
+    let employee = {
+      name: 'John Doe',
+      email: 'johndoe@email.com',
+      baseSalary: 5000.00,
+      post: 'Developer'
+    }
 
+    let salary = calculator.getSalary( employee );
+
+    expect( salary ).toBeDeepCloseTo( 4000.00, 2 );
   });
 
   
