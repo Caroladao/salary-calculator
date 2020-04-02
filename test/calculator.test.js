d0dda27bcbefdabeb3111c523c733d4088e03afe
Carol Adão <caroline@forlogic.net>
2020-04-01 22:37:13 -0300

    test coverage 100% DBA

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -6,27 +6,51 @@ const calculator = new Calculator();
 
 describe( 'Calculator', () => {
   test( 'Developer with base salary less than 3000', () => {
-    let employee = {
+    const employee = {
       name: 'John Doe',
       email: 'johndoe@email.com',
       baseSalary: 2000.00,
       post: 'Developer'
     }
     
-    let salary = calculator.getSalary( employee );
+    const salary = calculator.getSalary( employee );
     expect( salary ).toBeDeepCloseTo( 1800.00, 2 );
   });
 
   test( 'Developer with base salary greater than 3000', () => {
-    let employee = {
+    const employee = {
       name: 'John Doe',
       email: 'johndoe@email.com',
       baseSalary: 5000.00,
       post: 'Developer'
     }
     
-    let salary = calculator.getSalary( employee );
+    const salary = calculator.getSalary( employee );
     expect( salary ).toBeDeepCloseTo( 4000.00, 2 );
+  });
+
+  test( 'DBA with base salary less than 2000', () => {
+    const employee = {
+      name: 'Maria Lopez',
+      email: 'marialopez@email.com',
+      baseSalary: 1500.00,
+      post: 'DBA'
+    }
+    
+    const salary = calculator.getSalary( employee );
+    expect( salary ).toBeDeepCloseTo( 1275.00, 2 );
+  });
+
+  test( 'Developer with base salary greater than 2000', () => {
+    const employee = {
+      name: 'Maria Lopez',
+      email: 'marialopez@email.com',
+      baseSalary: 3500.00,
+      post: 'DBA'
+    }
+    
+    const salary = calculator.getSalary( employee );
+    expect( salary ).toBeDeepCloseTo( 2625.00, 2 );
   });
 
   
