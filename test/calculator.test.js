4dce6dfea0aefe661a927b981f12d28f565d4b2d
Carol Adão <caroline@forlogic.net>
2020-04-01 22:31:14 -0300

    test coverage 100% developer

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -5,16 +5,27 @@ expect.extend({ toBeDeepCloseTo })
 const calculator = new Calculator();
 
 describe( 'Calculator', () => {
-  test( '', () => {
+  test( 'Developer with base salary less than 3000', () => {
+    let employee = {
+      name: 'John Doe',
+      email: 'johndoe@email.com',
+      baseSalary: 2000.00,
+      post: 'Developer'
+    }
+    
+    let salary = calculator.getSalary( employee );
+    expect( salary ).toBeDeepCloseTo( 1800.00, 2 );
+  });
+
+  test( 'Developer with base salary greater than 3000', () => {
     let employee = {
       name: 'John Doe',
       email: 'johndoe@email.com',
       baseSalary: 5000.00,
       post: 'Developer'
     }
-
+    
     let salary = calculator.getSalary( employee );
-
     expect( salary ).toBeDeepCloseTo( 4000.00, 2 );
   });
 
